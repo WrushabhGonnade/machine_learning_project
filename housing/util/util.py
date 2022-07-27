dc6d7eed1075ca914a551c8decc9a4e7d4270bfe
WrushabhGonnade <87172076+WrushabhGonnade@users.noreply.github.com>
2022-07-27 16:25:35 +0530

    config.yaml, configuration.py etc added

---
+++ housing/util/util.py
@@ -0,0 +1,12 @@
+import yaml
+from housing.exception import HousingException
+import os,sys
+
+# util.py contains all the helper functions such as reading yaml file, pickling and all
+
+def read_yaml_file(file_path:str) -> dict:
+    try:
+        with open(config_file_path,"rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise HousingException(e,sys) from e
