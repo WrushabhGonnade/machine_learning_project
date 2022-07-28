e1af80e9eb489c381f0291f6b4c7b58b197a1b1e
WrushabhGonnade <87172076+WrushabhGonnade@users.noreply.github.com>
2022-07-28 22:55:20 +0530

    updated Data Validation Config in Configuration

--- housing/util/util.py
+++ housing/util/util.py
@@ -1,12 +1,13 @@
 import yaml
 from housing.exception import HousingException
 import os,sys
+from housing.constant import *
 
 # util.py contains all the helper functions such as reading yaml file, pickling and all
 
-def read_yaml_file(file_path:str) -> dict:
+def read_yaml_file(file_path:str)->dict:
     try:
-        with open(config_file_path,"rb") as yaml_file:
+        with open(file_path, 'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
     except Exception as e:
         raise HousingException(e,sys) from e
