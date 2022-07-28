661fb482b26c203a1ff9fb6eaeda0eb568a3a184
WrushabhGonnade <87172076+WrushabhGonnade@users.noreply.github.com>
2022-07-28 14:41:00 +0530

    commited data

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -1,11 +1,21 @@
 import imp
 from housing.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig, TrainingPipelineConfig
 from housing.util.util import read_yaml_file
+from housing.constant import *
+from housing.exception import HousingException
+import sys
+
+
+
 
 
 class Configuration:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+    config_file_path:str=CONFIG_FILE_PATH,
+    current_time_stamp:str=CURRENT_TIME_STAMP) -> None:
+        self.config_info=read_yaml_file(file_path=config_file_path)
+        self.training_pipeline_config=self.get_training_pipeline_config()
+        self.time_stamp=current_time_stamp
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         pass
@@ -26,4 +36,7 @@ class Configuration:
         pass
 
     def get_training_pipeline_config(self) -> TrainingPipelineConfig:
-        pass
+        try:
+            training_pipeline_config=self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+        except Exception as e:
+            raise HousingException(e,sys) from e
