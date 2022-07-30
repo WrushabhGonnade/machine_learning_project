5aafe639737bd32189a396ce0e4a05d39dc84c5d
WrushabhGonnade <87172076+WrushabhGonnade@users.noreply.github.com>
2022-07-31 00:46:48 +0530

    updated exception.py file

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -29,6 +29,7 @@ class Configuration:
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         try:
+            #raise Exception("Testing Exception")
             artifact_dir=self.training_pipeline_config.artifact_dir
             data_ingestion_artifact_dir=os.path.join(artifact_dir,DATA_INGESTION_ARTIFACT_DIR, self.time_stamp)
             data_ingestion_info=self.config_info[DATA_INGESTION_CONFIG_KEY]
