9c11afa507b6cbdfd0a17e1f6d70f6fb1af8e343
WrushabhGonnade <87172076+WrushabhGonnade@users.noreply.github.com>
2022-08-01 00:41:03 +0530

    added schema.yaml

--- housing/component/data_ingestion.py
+++ housing/component/data_ingestion.py
@@ -71,7 +71,8 @@ class DataIngestion:
 
             logging.info(f"Reading csv file: [{housing_file_path}]")
             housing_data_frame = pd.read_csv(housing_file_path)
-
+            
+            # Below line is crreating a new column in dataframe with name income_cat
             housing_data_frame["income_cat"] = pd.cut(
                 housing_data_frame["median_income"],
                 bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
